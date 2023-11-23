a5e92dbe5a6c2394358bbfb15b4a98c848d81090
MBBrunito <tecnolastipas@gmail.com>
2023-11-23 17:20:45 -0300

    Arreglando Estilos

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,12 +6,14 @@ export default function LandigPage() {
    const logo = "./Logo.jpeg";
    return (
       <div>
-         <h1 className="titulo">MENTALIS</h1>
-         <img
-            src="https://res.cloudinary.com/daoavxvau/image/upload/v1699902288/Mentalis/logo_l8vwnw.png"
-            alt="Logo Mentalis"
-            className="logoLandingPage"
-         />
+         <div className="contenedor">
+            <h1 className="titulo">MENTALIS</h1>
+            <img
+               src="https://res.cloudinary.com/daoavxvau/image/upload/v1699902288/Mentalis/logo_l8vwnw.png"
+               alt="Logo Mentalis"
+               className="logoLandingPage"
+            />
+         </div>
          <div>
             <Footer />
          </div>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,51 +3,55 @@ import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
    faSquareFacebook,
-   faSquareInstagram,
-   faSquareWhatsapp,
+   faInstagram,
+   faWhatsapp,
 } from "@fortawesome/free-brands-svg-icons";
+import Link from "next/link";
 
 const Footer = () => {
    return (
       <footer>
          <div className="footer">
-            <div>
-               <a href="/">
+            <div className="contLogo">
+               <Link href="/">
+                  {/* {!isHome && ( */}
                   <Image
-                     src="https://res.cloudinary.com/daoavxvau/image/upload/v1699902288/Mentalis/logo_l8vwnw.png"
-                     width={50}
-                     height={50}
+                     src="https://res.cloudinary.com/daoavxvau/image/upload/v1700765805/Mentalis/r2koo4vquo5rzxra57vq.png"
+                     width={30}
+                     height={30}
                      alt="foto del logo"
-                     className="logoFooter"
+                     className="logoInicio"
                   />
-               </a>
+                  {/* )} */}
+               </Link>
             </div>
             <div>
                <h3>&copy; Mentalis Psicologia y Humanidades</h3>
-               <p>2023 - {new Date().getFullYear()}</p>
-               <p>All rights Reserved.</p>
+               <p>All rights Reserved • 2023-{new Date().getFullYear()}</p>
             </div>
-            <a
-               href="https://www.facebook.com/mentalispyh"
-               target="_blank"
-               rel="noopener noreferrer"
-            >
-               <FontAwesomeIcon className="icon" icon={faSquareFacebook} />
-            </a>
-            <a
-               href="https://www.instagram.com/mentalispyh/"
-               target="_blank"
-               rel="noopener noreferrer"
-            >
-               <FontAwesomeIcon className="icon" icon={faSquareInstagram} />
-            </a>
-            <a
-               href="https://wa.me/56977499792"
-               target="_blank"
-               rel="noopener noreferrer"
-            >
-               <FontAwesomeIcon className="icon" icon={faSquareWhatsapp} />
-            </a>
+            <div className="redes">
+               <a
+                  href="https://www.facebook.com/mentalispyh"
+                  target="_blank"
+                  rel="noopener noreferrer"
+               >
+                  <FontAwesomeIcon className="icon" icon={faSquareFacebook} />
+               </a>
+               <a
+                  href="https://www.instagram.com/mentalispyh/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+               >
+                  <FontAwesomeIcon className="icon" icon={faInstagram} />
+               </a>
+               <a
+                  href="https://wa.me/56977499792"
+                  target="_blank"
+                  rel="noopener noreferrer"
+               >
+                  <FontAwesomeIcon className="icon" icon={faWhatsapp} />
+               </a>
+            </div>
          </div>
       </footer>
    );
