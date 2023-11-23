06c44fb1e45914f5ddcf12b4ce5bc0ea267f2bbe
MBBrunito <tecnolastipas@gmail.com>
2023-11-23 17:30:29 -0300

    Medidas pantallas responsive

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,26 +7,28 @@ export default function LandigPage() {
    return (
       <div>
          <div className="contenedor">
-            <h1 className="titulo">MENTALIS</h1>
-            <img
-               src="https://res.cloudinary.com/daoavxvau/image/upload/v1699902288/Mentalis/logo_l8vwnw.png"
-               alt="Logo Mentalis"
-               className="logoLandingPage"
-            />
+            <div className="contImagen">
+               <h1 className="titulo">MENTALIS</h1>
+               <img
+                  src="https://res.cloudinary.com/daoavxvau/image/upload/v1699902288/Mentalis/logo_l8vwnw.png"
+                  alt="Logo Mentalis"
+                  className="logoLandingPage"
+               />
+            </div>
+            <div>
+               <h4>
+                  Eslogan de la empresa, ejemplo: Estamos para ayudarte en lo
+                  que necesites
+               </h4>
+               <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Possimus fuga veritatis dignissimos amet soluta suscipit,
+                  similique deserunt voluptates praesentium! Excepturi nobis
+                  accusamus voluptate sint sit quidem, corrupti ex odio qui.
+               </p>
+            </div>
          </div>
-         <div>
-            <h4>
-               Eslogan de la empresa, ejemplo: Estamos para ayudarte en lo que
-               necesites
-            </h4>
-            <p>
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
-               fuga veritatis dignissimos amet soluta suscipit, similique
-               deserunt voluptates praesentium! Excepturi nobis accusamus
-               voluptate sint sit quidem, corrupti ex odio qui.
-            </p>
-            <Footer />
-         </div>
+         <Footer />
       </div>
    );
 }
