27abc2cda5d2c0f14b2647e3d872c96b8827ab39
Auliorp <aulioroveroparedes@gmail.com>
2023-11-30 23:25:58 -0300

    cambio de prueba

--- src/app/servicios/[id]/page.jsx
+++ src/app/servicios/[id]/page.jsx
@@ -1,11 +1,9 @@
-import Footer from "@/components/Footer/Footer";
 import ServiceDetail from "../../../components/Service/ServiceDetail";
 
 const serviceId = () => {
    return (
       <div>
          <ServiceDetail />
-         <Footer />
       </div>
    );
 };
