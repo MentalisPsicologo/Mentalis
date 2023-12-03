0d4da65b252b90dc0615750f93d186bd94344b3f
MBBrunito <tecnolastipas@gmail.com>
2023-12-03 11:23:07 -0300

    Form y Estilos

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,13 +7,11 @@ import Image from "next/image";
 
 const NavBar = () => {
    const pathname = usePathname();
-   const isHome = pathname === "/";
 
    return (
       <div className="contNav">
          <nav className="navBar ">
             <Link href="/" className="contLogo">
-               {/* {!isHome && ( */}
                <Image
                   src="https://res.cloudinary.com/daoavxvau/image/upload/v1700765805/Mentalis/r2koo4vquo5rzxra57vq.png"
                   width={40}
@@ -21,11 +19,16 @@ const NavBar = () => {
                   alt="foto del logo"
                   className="logoInicio"
                />
-               {/* )} */}
             </Link>
             <div className="menus">
                {dataNavbar.map((item, index) => (
-                  <Link className="boton" key={index} href={item.url}>
+                  <Link
+                     className={`boton ${
+                        pathname === item.url ? "activo" : ""
+                     }`}
+                     key={index}
+                     href={item.url}
+                  >
                      {item.name}
                   </Link>
                ))}
---
+++ src/components/PhoneNumberValidation/PhoneNumberValidation.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
+
+export default function PhoneNumberValidation() {
+   const [PhoneNumber, setPhoneNumber] = useState("");
+   const [isValid, setIsValid] = useState(true);
+
+   const handleChange = (value) => {
+      setPhoneNumber(value);
+      setIsValid(validatePhoneNumber(value));
+   };
+
+   const validatePhoneNumber = (phoneNumber) => {
+      const phoneNumberPattern = /^\d{10,12}$/;
+      return phoneNumberPattern.test(phoneNumber);
+   };
+
+   return (
+      <div className="phoneNumber">
+         <PhoneInput
+            country="mx"
+            type="text"
+            value={PhoneNumber}
+            onChange={handleChange}
+            inputProps={{ required: true }}
+            containerClass="custom-container-class"
+            inputClass="custom-input-class"
+         />
+         {!isValid && (
+            <p className="formErrors">El númeron ingresado no es correcto</p>
+         )}
+      </div>
+   );
+}
