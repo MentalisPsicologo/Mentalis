6e7d9b488ef7e66442c58e811d08386fba2a31fb
MBBrunito <tecnolastipas@gmail.com>
2024-01-03 22:03:05 -0300

    Configurando Cors Next.Js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   //esta parte de aca permite usar la etiqueta Image
+   // Esta parte de acá permite usar la etiqueta Image
    images: {
       domains: ["i.pinimg.com", "res.cloudinary.com"],
    },
 };
 
-module.exports = nextConfig;
+module.exports = {
+   ...nextConfig,
+   async headers() {
+      return [
+         {
+            // Matching all API routes
+            source: "/api/:path*",
+            headers: [
+               { key: "Access-Control-Allow-Credentials", value: "true" },
+               { key: "Access-Control-Allow-Origin", value: "*" },
+               {
+                  key: "Access-Control-Allow-Methods",
+                  value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+               },
+               {
+                  key: "Access-Control-Allow-Headers",
+                  value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+               },
+            ],
+         },
+      ];
+   },
+};
